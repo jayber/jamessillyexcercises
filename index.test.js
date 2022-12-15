e727cc668a79bfa606dcc41a8e39ae08338a5f18
james <james.a.bromley@gmail.com>
2022-12-15 16:14:59 +0000

    something

--- index.test.js
+++ index.test.js
@@ -1,3 +1,13 @@
+/**
+ * Read the following code and decide whether the test will pass or fail
+ * From the project root
+ * Run `npm install` 
+ * Run `npm run test`
+ * Did it do what you expect? If not follow the steps in 
+ * https://docs.google.com/document/d/1MENAmlQ2nCvsLEg4nV0YL2D5rD5se1_ke2fjAIX5HEQ/edit?usp=sharing
+ * to help you understand why. 
+ */
+
 let dog = "Burt The Dog";
 let cat = "Alfred The Cat"
 
