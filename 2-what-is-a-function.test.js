a4152fe73308ae284f35b8e417dccb05c6d2bdb2
james <james.a.bromley@gmail.com>
2022-12-19 20:50:14 +0000

    update

--- 2-what-is-a-function.test.js
+++ 2-what-is-a-function.test.js
@@ -24,12 +24,15 @@ let anotherArrowFunction = x => {
     return "ran another arrow function with parameter value: " + x;
 }
 
-//change the argument passed to toEquals()
+//fill in the correct string argument passed to toEquals()
 test("myFunction", () => {expect(myFunction()).toEqual("")});
 test("firstFunction", () => {expect(firstFunction()).toEqual("")});
 test("secondFunction", () => {expect(secondFunction()).toEqual("")});
 test("arrowFunction", () => {expect(arrowFunction()).toEqual("")});
 test("anotherArrowFunction", () => {expect(anotherArrowFunction("arg 1")).toEqual("")});
+
+/* Once you have those working, uncomment this section and get the second
+set of tests to run
 
 //don't change this
 //now mess things up!
@@ -39,9 +42,10 @@ secondFunction = arrowFunction;
 arrowFunction = anotherArrowFunction;
 anotherArrowFunction = spare; 
 
-//change the argument passed to toEquals()
-test("myFunction", () => {expect(myFunction()).toEqual("")});
-test("firstFunction", () => {expect(firstFunction()).toEqual("")});
-test("secondFunction", () => {expect(secondFunction()).toEqual("")});
-test("arrowFunction", () => {expect(arrowFunction()).toEqual("")});
-test("anotherArrowFunction", () => {expect(anotherArrowFunction("arg 1")).toEqual("")});
+//fill in the correct string argument passed to toEquals()
+test("myFunction2", () => {expect(myFunction()).toEqual("")});
+test("firstFunction2", () => {expect(firstFunction()).toEqual("")});
+test("secondFunction2", () => {expect(secondFunction()).toEqual("")});
+test("arrowFunction2", () => {expect(arrowFunction()).toEqual("")});
+test("anotherArrowFunction2", () => {expect(anotherArrowFunction("arg 1")).toEqual("")});
+*/
