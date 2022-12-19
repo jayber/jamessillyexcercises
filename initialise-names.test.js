23449a5fda13638f3ba55ecbb209b232f5bd3044
james <james.a.bromley@gmail.com>
2022-12-19 16:29:17 +0000

    update names exercise

--- initialise-names.test.js
+++ initialise-names.test.js
@@ -4,6 +4,19 @@
  * its initial letters, separated by a comma.
  * Eventually the test here should pass, but you should
  * write your own tests on the way
+ * 
+ * This is about breaking down the problem into smaller problems
+ * and solving them one at a time.
+ * 
+ * First write a test for a smaller part of the problem, then write
+ * code that makes that test pass, then write another test, that either
+ * tests a separate part of the problem, or the combination of parts, etc.
+ * 
+ * Given a problem that is about doing the same thing on every element
+ * of an array, what would be a smaller sub-problem on the way?
+ * 
+ * How do you separate the code for each sub-problem so that you can test
+ * it individually?
  */
 
 
